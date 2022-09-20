d647fec0d362652abb461f3cb2cfcb17d36d3c88
ruttalaPavankumar123456 <111498373+ruttalaPavankumar123456@users.noreply.github.com>
2022-09-20 19:28:25 +0530

    x=str(input()) vowel=['a','e','i','o','u'] digits=['0','1','2','3','4','5','6','7','8','9'] space=' ' v=c=d=s=0 for i in range (len(x)):     if x[i] in vowel:         v+=1     elif x[i] in digits:         d+=1     elif x[i] in space:         s+=1     else:         c+=1 print('Vowels:',v) print('Consonants:',c) print('Digits:',d) print('White spaces:',s)

---
+++ Count_no._of_vowels_,consonants_,digits_and_spaces.py
@@ -0,0 +1,18 @@
+x=str(input())
+vowel=['a','e','i','o','u']
+digits=['0','1','2','3','4','5','6','7','8','9']
+space=' '
+v=c=d=s=0
+for i in range (len(x)):
+    if x[i] in vowel:
+        v+=1
+    elif x[i] in digits:
+        d+=1
+    elif x[i] in space:
+        s+=1
+    else:
+        c+=1
+print('Vowels:',v)
+print('Consonants:',c)
+print('Digits:',d)
+print('White spaces:',s)
