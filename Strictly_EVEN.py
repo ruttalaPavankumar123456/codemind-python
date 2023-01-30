3dee6f56ef8559ecab85ea0ae9c5671f62b3d1ae
ruttalaPavankumar123456 <111498373+ruttalaPavankumar123456@users.noreply.github.com>
2023-01-30 18:04:48 +0530

    n=int(input()) m=list(map(int,input().split())) a=0 b=0 for j in range(n):     if j%2==0 and m[j]%2==0:         a+=1     if m[j]%2==0:         b+=1 if a==b:     print(True) else:     print(False)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,13 @@
+n=int(input())
+m=list(map(int,input().split()))
+a=0
+b=0
+for j in range(n):
+    if j%2==0 and m[j]%2==0:
+        a+=1
+    if m[j]%2==0:
+        b+=1
+if a==b:
+    print(True)
+else:
+    print(False)
