a27a0746169a44b4c23d5226fc68094e7e41dd7a
ruttalaPavankumar123456 <111498373+ruttalaPavankumar123456@users.noreply.github.com>
2023-02-05 16:23:28 +0530

    def prm(a):     if a>1:         cnt=0         for j in range(2,a):             if a%j==0:                 cnt+=1         if cnt==0:             return(a) n=int(input()) z=list(map(int,input().split())) k=int(input()) cnt=0 for j in z:     if prm(j)==j and j>=k:         cnt+=1 print(cnt)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,16 @@
+def prm(a):
+    if a>1:
+        cnt=0
+        for j in range(2,a):
+            if a%j==0:
+                cnt+=1
+        if cnt==0:
+            return(a)
+n=int(input())
+z=list(map(int,input().split()))
+k=int(input())
+cnt=0
+for j in z:
+    if prm(j)==j and j>=k:
+        cnt+=1
+print(cnt)
